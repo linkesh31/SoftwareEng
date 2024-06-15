a2b983f9599dac13e359da403d7ecbdd6099c82c
PookieChipss <101377503+PookieChipss@users.noreply.github.com>
2024-06-15 14:52:25 +0800

    Delete Software_Project/Harvind/Harvind directory

--- Software_Project/Harvind/Harvind/connector/python_connect_to_mysql.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-conn=mysql.connector.connect(host='localhost',username='root',password='calladoctor1234',database='calladoctor')
-my_cursor=conn.cursor()
-
-conn.commit()
-conn.close()
-
-print("Connection succesfully created!")
