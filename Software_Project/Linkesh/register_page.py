3b7d03ee795231bc6e53ec3e8fb242b602a56c37
linkesh31 <101377498+linkesh31@users.noreply.github.com>
2024-06-18 20:38:36 +0800

    Update register_page.py

--- Software_Project/Linkesh/register_page.py
+++ Software_Project/Linkesh/register_page.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk, ImageDraw
 import mysql.connector
+import customtkinter as ctk
 
 def create_rounded_image(image_path, size, corner_radius):
     image = Image.open(image_path).resize(size, Image.Resampling.LANCZOS)
